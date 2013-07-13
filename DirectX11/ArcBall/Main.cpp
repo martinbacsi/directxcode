71767bf3a75a4930b2cf72cec51569b9f32110b4
vckzdd@gmail.com <vckzdd@gmail.com@285dc577-2737-961f-e29d-756989566dd9>
2013-07-13 14:00:49 +0000

    change vertex coordinates

--- DirectX11/ArcBall/Main.cpp
+++ DirectX11/ArcBall/Main.cpp
@@ -144,14 +144,14 @@ VOID InitVertexBuffer()
 	// Create the vertex buffer
 	SimpleVertex vertices[] = 
 	{
-		{ XMFLOAT3( -1.0f, 1.0f, -1.0f ), XMFLOAT3( 0.0f, 0.0f, 1.0f) },
-        { XMFLOAT3( 1.0f,  1.0f, -1.0f ), XMFLOAT3( 0.0f, 1.0f, 0.0f) },
-        { XMFLOAT3( 1.0f,  1.0f,  1.0f ), XMFLOAT3( 0.0f, 1.0f, 1.0f) },
-        { XMFLOAT3(-1.0f,  1.0f,  1.0f ), XMFLOAT3( 1.0f, 0.0f, 0.0f) },
-        { XMFLOAT3(-1.0f, -1.0f, -1.0f ), XMFLOAT3( 1.0f, 0.0f, 1.0f) },
-        { XMFLOAT3( 1.0f, -1.0f, -1.0f ), XMFLOAT3( 1.0f, 1.0f, 0.0f) },
-        { XMFLOAT3( 1.0f, -1.0f,  1.0f ), XMFLOAT3( 1.0f, 1.0f, 1.0f) },
-        { XMFLOAT3(-1.0f, -1.0f,  1.0f ), XMFLOAT3( 0.0f, 0.0f, 0.0f) },
+		{ XMFLOAT3(-10.0f,  10.0f, -10.0f ), XMFLOAT3( 0.0f, 0.0f, 1.0f) },
+        { XMFLOAT3( 10.0f,  10.0f, -10.0f ), XMFLOAT3( 0.0f, 1.0f, 0.0f) },
+        { XMFLOAT3( 10.0f,  10.0f,  10.0f ), XMFLOAT3( 0.0f, 1.0f, 1.0f) },
+        { XMFLOAT3(-10.0f,  10.0f,  10.0f ), XMFLOAT3( 1.0f, 0.0f, 0.0f) },
+        { XMFLOAT3(-10.0f, -10.0f, -10.0f ), XMFLOAT3( 1.0f, 0.0f, 1.0f) },
+        { XMFLOAT3( 10.0f, -10.0f, -10.0f ), XMFLOAT3( 1.0f, 1.0f, 0.0f) },
+        { XMFLOAT3( 10.0f, -10.0f,  10.0f ), XMFLOAT3( 1.0f, 1.0f, 1.0f) },
+        { XMFLOAT3(-10.0f, -10.0f,  10.0f ), XMFLOAT3( 0.0f, 0.0f, 0.0f) },
 	};
 
 	// Vertex Buffer
