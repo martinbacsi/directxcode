75f336964481b2aaf5c56060cba3f0dfa298690d
vckzdd@gmail.com <vckzdd@gmail.com@285dc577-2737-961f-e29d-756989566dd9>
2013-07-18 13:52:10 +0000

    Change vertex index to clockwise order

--- DirectX10/ShaderWithoutBuffers/shader_without_buffers.hlsl
+++ DirectX10/ShaderWithoutBuffers/shader_without_buffers.hlsl
@@ -26,9 +26,9 @@ VS_OUTPUT VS ( VS_INPUT input)
 	if (input.vertexId == 0)
 		output.pos = float4(0.0, 0.5, 0.5, 1.0);
 	else if (input.vertexId == 1)
-		output.pos = float4(-0.5, -0.5, 0.5, 1.0);
-	else
 		output.pos = float4(0.5, -0.5, 0.5, 1.0);
+	else
+		output.pos = float4(-0.5, -0.5, 0.5, 1.0);
 
 	output.color = clamp(output.pos, 0, 1);
 
